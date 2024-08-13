dee1cea6d3815f7c1f52314ebf2ff9fb1987a0d5
sebastian-nunez <02snunez@gmail.com>
2024-08-13 18:45:57 -0400

    refactor: moves switch into handleMessage

--- cmd/main.go
+++ cmd/main.go
@@ -26,47 +26,46 @@ func main() {
 			logger.Printf("Error decoding message: %v", err)
 			continue
 		}
-
-		switch method {
-		case "initialize":
-			var request lsp.InitializeRequest
-			if err := json.Unmarshal(content, &request); err != nil {
-				logger.Printf("Error unmarshalling initialize request: %v", err)
-				continue
-			}
-
-			version := "unknown"
-			if request.Params.ClientInfo != nil && *request.Params.ClientInfo.Version != "" {
-				version = *request.Params.ClientInfo.Version
-			}
-			logger.Printf("Connected to client: %s (version=%s)",
-				request.Params.ClientInfo.Name,
-				version,
-			)
-
-			// TODO(sebastian-nunez): refactor to just use the writer directly
-			writer := os.Stdout
-			msg := rpc.EncodeMessage(lsp.NewInitializeResponse(request.ID, 1))
-			writer.Write([]byte(msg))
-
-			logger.Printf("Sent initialize response: %v", string(msg))
-		case "textDocument/didOpen":
-			var request lsp.DidOpenTextDocumentNotification
-			if err := json.Unmarshal(content, &request); err != nil {
-				logger.Printf("Error unmarshalling text document did open request: %v", err)
-				continue
-			}
-
-			logger.Printf("Opened text document: URI=%v, content=%v",
-				request.Params.TextDocument.URI,
-				request.Params.TextDocument.Text,
-			)
-		default:
-			handleMessage(logger, method, content)
-		}
+		handleMessage(logger, method, content)
 	}
 }
 
 func handleMessage(logger *log.Logger, method string, content []byte) {
-	logger.Printf("Received message: method=%v, content=%v", method, string(content))
+	switch method {
+	case "initialize":
+		var request lsp.InitializeRequest
+		if err := json.Unmarshal(content, &request); err != nil {
+			logger.Printf("Error unmarshalling initialize request: %v", err)
+			break
+		}
+
+		version := "unknown"
+		if request.Params.ClientInfo != nil && *request.Params.ClientInfo.Version != "" {
+			version = *request.Params.ClientInfo.Version
+		}
+		logger.Printf("Connected to client: %s (version=%s)",
+			request.Params.ClientInfo.Name,
+			version,
+		)
+
+		// TODO(sebastian-nunez): refactor to just use the writer directly
+		writer := os.Stdout
+		msg := rpc.EncodeMessage(lsp.NewInitializeResponse(request.ID, 1))
+		writer.Write([]byte(msg))
+
+		logger.Printf("Sent initialize response: %v", string(msg))
+	case "textDocument/didOpen":
+		var request lsp.DidOpenTextDocumentNotification
+		if err := json.Unmarshal(content, &request); err != nil {
+			logger.Printf("Error unmarshalling text document did open request: %v", err)
+			break
+		}
+
+		logger.Printf("Opened text document: URI=%v, content=%v",
+			request.Params.TextDocument.URI,
+			request.Params.TextDocument.Text,
+		)
+	default:
+		logger.Printf("Received message: method=%v, content=%v", method, string(content))
+	}
 }
